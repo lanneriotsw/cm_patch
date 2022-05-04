aad8b78ae811ce1f08d2d82dc06d6278e5222ab3
UFO Chen <jrchen982@gmail.com>
2022-05-04 18:37:21 +0800

    Update CM version to 1.2.0

--- update.sh
+++ update.sh
@@ -1,10 +1,9 @@
 #!/bin/env bash
-echo "Starting upgrade CM from v1.1.0 to v1.1.1"
-curl -JLO https://link.lannerinc.com/api/v1/cm/download?version=1.1.1
+echo "Start upgrading CM"
+curl -JLO https://link.lannerinc.com/api/v1/cm/download?version=1.2.0
 sudo apt -y remove cmservice
-sudo apt -y install ./cmservice_1.1.1_amd64.deb
+sudo apt -y --allow-downgrades install ./cmservice_1.2.0_amd64.deb
 docker stop cmsfrontend
 docker rm cmsfrontend
-docker rmi lanneriotsw/cms-frontend
 docker run --name cmsfrontend --net host --restart unless-stopped -d lanneriotsw/cms-frontend
-echo "Complete"
+echo "Upgrade completed"
