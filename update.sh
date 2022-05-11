a016a2569d0ffe81614e6768c460d0929d5422d6
Lanner IoT Software <62928413+lanneriotsw@users.noreply.github.com>
2022-05-11 15:07:26 +0800

    Add pull new docker image command

--- update.sh
+++ update.sh
@@ -5,5 +5,6 @@ sudo apt -y remove cmservice
 sudo apt -y --allow-downgrades install ./cmservice_1.2.0_amd64.deb
 docker stop cmsfrontend
 docker rm cmsfrontend
+docker pull lanneriotsw/cms-frontend
 docker run --name cmsfrontend --net host --restart unless-stopped -d lanneriotsw/cms-frontend
 echo "Upgrade completed"
